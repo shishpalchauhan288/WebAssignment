bb408f96b4030890e9eef9bad716d1b72f90fbc3
shishpal <shishpalchauhan288@gail.com>
2022-11-05 18:40:57 +0530

    onclick event comlete

---
+++ 4-Advance-JS/410-Projects/AdvanceJS-410-01/script.js
@@ -0,0 +1,20 @@
+ const searchuser = (e) => {
+    e.preventDefault();
+    var search = document.getElementById("search").value;
+    // document.getElementById('result')=
+
+    fetch("https://api.github.com/users/"  + search).then((result)=>result.json()).then((data)=>{
+
+        document.getElementsById('result').innerHTML = 
+        ` <a target="_blank href=""https://api.github.com/${search}">  
+        <img src = ${data.avatar_url }  alt= "No user found" /> <a/>`;
+
+    })
+       
+        
+    
+
+    ;
+ }
+let form = document.getElementById("myForm");
+form.addEventListener('submit', searchuser);
