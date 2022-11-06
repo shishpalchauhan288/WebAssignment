b4dddd7ed61f9620e609f51acd5e0f14e5540051
shishpal <shishpalchauhan288@gail.com>
2022-11-06 20:18:44 +0530

    shopping list 402-03

---
+++ 4-Advance-JS/402-DOM-Manipulation/AdvanceJS-402-03/402-03.js
@@ -0,0 +1,28 @@
+function addMore(){
+    let GivenName = document.getElementById("tboxName").value;
+    let box = document.getElementById("box");
+
+    let li = document.createElement('li');
+    li.textContent = GivenName;
+
+    let a = document.createElement('a');
+   a.textContent = "Delete";
+   a.style.textDecoration = "none";
+   li.appendChild(a);
+
+   let pos = box.firstElementChild;
+   if(pos==null){box.appendChild(li);}
+   else{box.insertBefore(li,pos)}
+
+   document.getElementById("tboxName").value = "";
+   document.getElementById("tboxName").focus();
+
+}
+
+let btn = document.querySelector('ul');
+btn.addEventListener('click', function(e){
+
+    let box = document.getElementById("box");
+    let li = e.target.parentNode;
+    box.removeChild(li);
+})
